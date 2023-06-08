b8a01ba69310968a7423ebf0c99bf057d4592446
Коваленко Сергей Юрьевич <sikovalenko@ncfu.ru>
2023-06-08 11:21:39 +0300

    test

---
+++ index.js
@@ -0,0 +1,29 @@
+// import ApexCharts from "apexcharts";
+
+// var options = {
+//   chart: {
+//     type: 'line'
+//   },
+// 	colors: ["#FF1654", "#247BA0"],
+//   series: [
+//     {
+//       name: "Series A",
+//       data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
+//     },
+//     {
+//       name: "Series B",
+//       data: [20, 29, 37, 36, 44, 45, 50, 58, 22, 45, 15]
+//     }
+//   ],
+
+//   xaxis: {
+//     categories: [30,05,6,7,8,9,10,11,12,13,14,15,16,17,18]
+//   },
+// 	stroke: {
+// 		curve: 'smooth',
+// 	}
+// }
+
+// var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+// chart.render();
